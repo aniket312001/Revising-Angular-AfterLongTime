1d9bb14733e0560aa8d5cfb8d810b541f1b6ebe8
aniket <aniketchavan505@gmail.com>
2022-01-26 14:31:14 +0530

    first commit

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,16 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map ,filter} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+ 
+
   constructor(private http : HttpClient) { }
 
   getData():Observable<any>{
     return this.http.get<any>("https://jsonplaceholder.typicode.com/users")
+  }
+
+  getMoreValue():Observable<any>{
+    return this.http.get<any>("http://localhost:3000/udata").pipe(map((val)=>{
+      console.log(val)
+        return val 
+    }),
+  //   map((val)=>{
+  //     console.log("2:",val)
+  //     return val.filter((val2:any) => val2.status === "active")
+  // }),
+  filter((val:any)=>{
+    
+    console.log(val,"2")
+    return val.length > 5
+  })
+  )
   }
 
 }
--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { map, filter,from, concatMap, toArray } from 'rxjs';
 import { UserService } from '../user.service';
 
 @Component({
@@ -9,11 +10,13 @@ import { UserService } from '../user.service';
 export class UserlistComponent implements OnInit {
 
   allData:any
+  val:any
   constructor(private service: UserService) { }
 
   ngOnInit(): void {
 
     this.getUserList()
+    this.getmore()
   }
 
   getUserList(){
@@ -21,6 +24,20 @@ export class UserlistComponent implements OnInit {
       this.allData = data
     })
   }
+  getmore(){
+    // this.service.getMoreValue().pipe(map((val)=>{
+    //       return val.filter((val2:any) => val2.status === "active")
+    //    }),).subscribe(data=>{
+    //   this.val = data
+    //   console.log(this.val)
+    // })
+                                      //  concatMap will make array in simple data    // toArray will make data to array
+    this.service.getMoreValue().pipe(concatMap(val=>val), filter((val:any)=>val.age>30),toArray()).subscribe(data=>{
+      this.val = data
+      console.log(this.val)
+    })
+  }
+  
 
 
 }
